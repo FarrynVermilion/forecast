46e379e2a5a9746eec043a8a5e93de16beefbde0
FarrynVermilion <150990057+FarrynVermilion@users.noreply.github.com>
2024-11-17 15:17:19 +0700

    Udah dibenerin

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
+# udah bener cuma kalo bisa benerin format output jadi Y doang bukan y m d
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.stattools import adfuller
@@ -9,12 +9,6 @@ pd.set_option('display.max_rows', None)
 
 data = pd.read_csv('data.csv', parse_dates=['Date'], index_col='Date')
 print(data)
-
-# plt.plot(data)
-# plt.title('Time Series Data')
-# plt.xlabel('Date')
-# plt.ylabel('ODA pacific total')
-# plt.show()
 
 result = adfuller(data['ODA pacific total'])
 print('ADF Statistic:', result[0])
@@ -28,12 +22,20 @@ q = 0  # Moving Average (MA) order
 model = ARIMA(data, order=(p, d, q))
 model_fit = model.fit()
 
-forecast_steps = 1
+forecast_steps = 5
 forecast = model_fit.forecast(steps=forecast_steps)
-print(forecast)
+
+latest_date = data.index.values[-1]
+s1 = [[latest_date, data['ODA pacific total'].iloc[-1]]]
+for x in forecast:
+    latest_date=pd.to_datetime(latest_date)+pd.DateOffset(years= 1)
+    s1.append([latest_date,x])
+df=pd.DataFrame(s1,columns=['Date','ODA Pacific'])
+df.set_index('Date', inplace=True)
+print(df)
 
 plt.plot(data, label='Original Data')
-plt.plot(range(len(data), len(data) + forecast_steps), forecast, color='red', label='Forecast')
+plt.plot(df , color='red', label='Forecast')
 plt.title('Time Series Forecast')
 plt.xlabel('Date')
 plt.ylabel('ODA pacific total')
